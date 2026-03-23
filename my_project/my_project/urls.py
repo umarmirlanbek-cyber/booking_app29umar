18a97cda2c8559ce1e7120fad1b562811067eaed
Aralbekov Umar <umarmirlanbek@gmail.com>
2026-03-23 16:19:33 +0600

    i added the Docker files

--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -1,7 +1,4 @@
-from django.contrib import admin
-from django.urls import path,include
 from django.conf import settings
-from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path,include
